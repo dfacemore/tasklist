5dd5e270e0539cc5abdb88e8f8a08f656feb8324
Anton Sarapin <dfaceonemore@gmail.com>
2024-07-03 07:57:02 +0300

    Updated Mappers

--- src/main/java/com/example/tasklist/web/mappers/TaskMapper.java
+++ src/main/java/com/example/tasklist/web/mappers/TaskMapper.java
@@ -1,10 +1,8 @@
 package com.example.tasklist.web.mappers;
 
-
 import com.example.tasklist.domain.task.Task;
 import com.example.tasklist.web.dto.task.TaskDto;
 import org.mapstruct.Mapper;
-
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper extends Mappable<Task, TaskDto> {
